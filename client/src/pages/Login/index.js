6fad3207935ac77a38350afa8f3de87ebd07fd5d
Marcelo Deus <marcelodeus98@gmail.com>
2022-06-17 12:57:16 -0300

    alter login page

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -11,7 +11,7 @@ const validateRegister = yup.object().shape({
   });
   const validateLogin = yup.object().shape({
     user: yup.string().required("O usuário é orbigátorio"),
-    password: yup.string().min(6, "A senha tem 6 caracteres no mínimo").required("A senha é obrigatória"),
+    password: yup.string().min(6, "A senha deve ter 6 caracteres no mínimo").required("A senha é obrigatória"),
   });
   
     function Home() {
@@ -48,71 +48,29 @@ const validateRegister = yup.object().shape({
       </section>  
 
           <div className="client-container">
-          <div style={{background:'#FFF', marginTop:20, padding:20, borderRadius:8, width:'auto'}}>
-            <div>
-              <h1 className='title-lgcontainer' style={{background:'#FFF', color:'#00008B'}}>Login</h1>
+          <div className="container-login" style={{ marginTop:20, padding:20, borderRadius:8, width:'auto'}}>
+              <h1 className='title-lgcontainer' style={{background:'transparent', color:'#FFF' ,marginBottom:40, padding:10}}>LOGIN</h1>
                   <Form onSubmit={handleLoginSubmit} class="login-form" schema={validateLogin}>
-                    <label>Usuário</label>
+                    <div className='container-login'>
+                    <label style={{}}>Usuário</label>
                     <div className="login-form-group">
-                      <Input name="user"  placeHolder="Informe seu usuário"/>
+                      <Input name="user"  required class="input" placeHolder="Informe seu usuário"/>
                     </div>
     
                     <label>Senha</label>
                     <div className="login-form-group">
-                      <Input name="password" type="password" className="form-field" placeHolder="Digite sua senha"/>
-    
+                      <Input name="password" type="password"  required class="input" placeHolder="Digite sua senha"/>
                     </div>
-                    
+
+                    </div>
                     <div className='container-buttons'>
-                      <button className="button btn" type="submit" style={{display:'flexbox', justifyContent:'center', alignItems:'center', marginTop:20, marginLeft:400}}>
+                      <button className="button btn" type="submit" style={{display:'flexbox', justifyContent:'center', alignItems:'center', marginTop:20, marginLeft:410}}>
                         Login
                       </button>
                     </div>
                   </Form>
-            </div>
           </div>
-
-          <div class="separator">
-              <div></div>
-              <h3 className='title-lgcontainer' style={{background:'#FFF', color:'#00008B' ,size:20}}>OU</h3>
-              <div></div>
-          </div>
-
-          <div style={{background:'#FFF', marginTop:20, padding:20, borderRadius:8, width:'auto'}}>
-            <div className="input-group">
-            <h1 className='title-lgcontainer' style={{background:'#FFF', color:'#00008B'}}>Cadastrar usuário</h1>
-                  <Form onSubmit={handleRegisterSubmit} schema={validateRegister} className="login-form">
-                    <label>Nome</label>
-                    <div className="login-form-group">
-                      <Input name="name" className="form-field" placeHolder="Digite seu nome"/>
-                    </div>
-                    
-                    <label>Usuário</label>
-                    <div className="login-form-group">
-                      <Input name="user" className="form-field" placeHolder="Informe o nome de usuário que deseja"/> 
-                    </div>
-    
-                    <label>Senha</label>
-                    <div className="login-form-group">
-                      <Input name="password" type="password" className="form-field" placeHolder="Informe a senha desejada"/>
-    
-                    </div>
-    
-                    <label>Confirma senha</label>
-                    <div className="login-form-group">
-                      <Input name="confirmPassword" type="password" className="form-field" placeHolder="Informe novamente a senha desejada"/>
-                    </div>
-                    
-                    <div className='container-buttons'>
-                      <button className="button" type="submit" style={{display:'flex', justifyContent:'center', alignItems:'center', marginTop:20, marginLeft:400}}>
-                        Registrar
-                      </button>
-                    </div>
-    
-                  </Form>
-            </div>
-          </div>  
-          </div>  
+        </div>  
       </>
       );    
   }
